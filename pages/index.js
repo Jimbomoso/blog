1b166be9b40c36f2ee21311740991fc30a7bab24
J.C <jimbomoso@gmail.com>
2021-01-20 15:15:06 -0500

    Adds intro/about parargaph

--- pages/index.js
+++ pages/index.js
@@ -28,6 +28,8 @@ export default function Home({ allPostsData }) {
         />
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h1> About this page </h1>
+        <p> A few years ago I fell in love with coding. I launched this page to document my journey in web development and to gain some experience maintaining my own blog. I wish I had done this from the very beginning, but better now than never as they say. My intent is for it to serve as a quick reference for me, while also serving as a showcase of some of my adventures as a web developer. I plan to keep the site basic to serve these purposes. The site itself was built using Next.js and deployed via Vercel. </p>
         <h2 className={utilStyles.headingLg}>quests</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
