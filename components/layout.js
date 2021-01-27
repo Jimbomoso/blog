c6d8bd9af55ff3cee882cf1474bb4a48672fdca0
J.C <jimbomoso@gmail.com>
2021-01-27 17:40:05 -0500

    Updates site title

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,7 @@ import { Follow } from 'react-twitter-widgets'
 
 const name = 'James Morrison'
 
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Adventures in Coding'
 
 export default function Layout({ children, home }) {
   return (
@@ -16,13 +16,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Development blog"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content="Web development blog"
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
