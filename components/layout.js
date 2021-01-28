d79df858f2b53ff623a6db11a823f3836e255a87
J.C <jimbomoso@gmail.com>
2021-01-28 15:00:18 -0500

    removes prism file extension

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 import { Follow } from 'react-twitter-widgets'
-import Prism from 'prismjs.js'
+import Prism from 'prismjs'
 import { useEffect } from 'react'
 
 const name = 'James Morrison'
